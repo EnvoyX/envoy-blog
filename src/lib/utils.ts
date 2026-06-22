d93656028ec18c72114d1a7c91ae35715359d07d
Muhamad Hanif Hafizhan <147917088+EnvoyX@users.noreply.github.com>
2026-06-22 14:13:08 +0700

    chore: migrate to cnfast

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,20 +1,16 @@
-import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-import { fromZonedTime } from 'date-fns-tz'
+import { fromZonedTime } from "date-fns-tz";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+export { cn } from "cnfast";
 
-const WIB_TZ = 'Asia/Jakarta'
+const WIB_TZ = "Asia/Jakarta";
 
 export function wibToUTC(date: Date) {
-  return fromZonedTime(date, WIB_TZ)
+  return fromZonedTime(date, WIB_TZ);
 }
 
 export const getCurrentDate = () => {
-  const now = new Date()
+  const now = new Date();
   //     const now = new Date("2026-01-26T00:00:00");
 
-  return now
-}
+  return now;
+};
